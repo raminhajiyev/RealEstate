7029f9eb60db10a2a90929c7136ce3d61eab8040
Ramin Haciyev <ramin.haciyev.2014@gmail.com>
2024-02-01 16:44:12 +0100

    Home Page View Components

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_ExploreVCPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _ExploreVCPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); } 
+    }
+}
